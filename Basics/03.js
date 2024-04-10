70a21164484b846dc955dbbb4f612891e099fbe8
praveen-1406 <124445308+praveen-1406@users.noreply.github.com>
2024-04-10 19:39:31 +0000

    new commit

---
+++ Basics/03.js
@@ -0,0 +1,20 @@
+// let score=null
+// console.log(score,typeof score)
+// let valueInNumber=Number(score)
+// console.log(valueInNumber,typeof valueInNumber)
+// let isLoggedIn=undefined
+// console.log(Boolean(isLoggedIn),typeof Boolean(isLoggedIn))
+// let sum=59
+// console.log(String(sum),typeof String(sum))
+
+// *************************************Operations******************************************
+// let value=3
+// let negValue=-value
+// console.log(value,negValue)
+// let num1=0,num2=9,num;
+// console.log(true+'k')
+// let num1=num2=num3=6
+// console.table([num1,num2,num3])
+let y=63
+console.log(y--)
+console.log(y)
