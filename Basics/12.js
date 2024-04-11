646bf89b0666685494d31a34d58a72ac441e4d88
praveen-1406 <124445308+praveen-1406@users.noreply.github.com>
2024-04-11 17:38:35 +0000

    Maths and Number

---
+++ Basics/12.js
@@ -0,0 +1,21 @@
+// const number = 596
+// console.log(number)
+// const balance = new Number(86696.595895)
+// console.log(balance.toString())
+// console.log(balance.toFixed(5), typeof balance.toFixed())
+// const precise = 126.5949;
+// console.log(precise.toPrecision(4))
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString())
+// console.log(hundreds.toLocaleString('en-IN'))
+//+++++++++++++++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++++++++++++++++++++++++
+console.log(Math) 
+console.log(Math.abs(5-6))  
+console.log(Math.round(5.79959))  
+console.log(Math.ceil(5.79959))  
+console.log(Math.floor(5.79959))  
+console.log(Math.min(4,8,65,4,5,7,5))  
+console.log(Math.max(4,8,65,4,5,7,5))  
+console.log(Math.random())
+const min=10,max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min)
